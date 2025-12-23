31fa82f210aed731deb0ceaf7cbb8a380a40a61a
Yaj Patel <yajpatel3594@gmail.com>
2025-12-23 23:25:18 +0530

    Update ALLOWED_HOSTS to allow all hosts in settings.py

--- compareproduct/compareproduct/settings.py
+++ compareproduct/compareproduct/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = 'django-insecure-jdiqw*sjp^g^4)y3f!mwbbkiv$rlt*0vaqrzb%@o71zre057$#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
